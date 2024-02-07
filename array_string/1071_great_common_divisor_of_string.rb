b878ecb4309fa2e17354b770ddd9f464cc9bfa88
Goh Matsumoto <urimaro@gmail.com>
2024-02-07 12:58:41 +0900

    Fix how to calcurate the multiple of the length of the substring in `str2`

--- array_string/1071_great_common_divisor_of_string.rb
+++ array_string/1071_great_common_divisor_of_string.rb
@@ -11,13 +11,14 @@ def gcd_of_strings(str1, str2)
     # GCD for the lengthｓ
     ## Verify whether the lengths of `str1` and `str2` are multiples of the length of the substring in `str2`.
     next unless length2 % count == 0
-    multiple2 = length2 / count
+    substr2_length = length2 / count
+    multiple2 = length2 / substr2_length
 
-    next unless length1 % multiple2 == 0
-    multiple1 = length1 / multiple2
+    next unless length1 % substr2_length == 0
+    multiple1 = length1 / substr2_length
 
     # GCD for the strings
-    substr2 = str2[0, multiple2]
+    substr2 = str2[0, substr2_length]
     ## "Verify if `str1` and `str2` are multiples of the substring found in `str2`."
     next unless (substr2 * multiple2) == str2
     return substr2 if (substr2 * multiple1) == str1
